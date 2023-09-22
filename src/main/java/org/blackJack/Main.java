24f088b3f9cf604f313ab0251faa570177ad2c6b
hajjou walkadi <walkadihajjou153@gmail.com>
2023-09-22 13:37:15 +0100

    creation des cartes

--- src/main/java/org/blackJack/Main.java
+++ src/main/java/org/blackJack/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BlackJack!!");
+        System.out.println("Welcome to BlackJaack!!");
 
         ArrayList<ArrayList<Integer>> allCartes = Carte.getALLCartes();
 
