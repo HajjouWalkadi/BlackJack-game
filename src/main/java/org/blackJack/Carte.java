849d9cf80413835b9f84f10db6a4bcf13fc8f731
hajjou walkadi <walkadihajjou153@gmail.com>
2023-09-22 12:24:50 +0100

    creation des cartes

--- src/main/java/org/blackJack/Carte.java
+++ src/main/java/org/blackJack/Carte.java
@@ -1,6 +1,17 @@
 package org.blackJack;
+import java.util.ArrayList;
 
 public class Carte {
-
-
+    public static ArrayList<ArrayList<Integer>> getALLCartes() {
+        ArrayList<ArrayList<Integer>> Cartes = new ArrayList<>();
+        for (int form = 1; form <= 4; form++) {
+            for (int i = 1; i <= 13; i++) {
+                ArrayList<Integer> card = new ArrayList<>();
+                card.add(i);
+                card.add(form);
+                Cartes.add(card);
+            }
+        }
+        return Cartes;
+    }
 }
--- src/main/java/org/blackJack/Main.java
+++ src/main/java/org/blackJack/Main.java
@@ -1,7 +1,16 @@
 package org.blackJack;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to BlackJack!!");
+
+        ArrayList<ArrayList<Integer>> allCartes = Carte.getALLCartes();
+
+        for (ArrayList<Integer> card : allCartes) {
+            System.out.println("Card: " + card.get(0) + " of Form: " + card.get(1));
+        }
     }
 }
+
