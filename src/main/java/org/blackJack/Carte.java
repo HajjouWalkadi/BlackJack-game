9b29874bf8751e37c88547ee42be0c23908fae22
hajjou walkadi <walkadihajjou153@gmail.com>
2023-09-27 16:41:23 +0100

    method extraire une carte done

--- src/main/java/org/blackJack/Carte.java
+++ src/main/java/org/blackJack/Carte.java
@@ -1,8 +1,12 @@
 package org.blackJack;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Arrays;
 
 public class Carte {
-    public static ArrayList<ArrayList<Integer>> getALLCartes() {
+
+   // method using ArrayList
+    /*public static ArrayList<ArrayList<Integer>> getALLCartes() {
         ArrayList<ArrayList<Integer>> Cartes = new ArrayList<>();
         for (int form = 1; form <= 4; form++) {
             for (int i = 1; i <= 13; i++) {
@@ -13,5 +17,44 @@ public class Carte {
             }
         }
         return Cartes;
+    }*/
+
+
+
+        public static int[][] getALLCartes() {
+            int[][] Cartes = new int[52][2];
+            int index = 0;
+            for (int form = 1; form <= 4; form++) {
+                for (int i = 1; i <= 13; i++) {
+                    Cartes[index][0] = i;
+                    Cartes[index][1] = form;
+                    index++;
+                }
+            }
+            return Cartes;
+        }
+
+
+    //methodes pour mélanger les cartes
+    public static Object[] extraire_ieme_carte(int[][] cartes, int indice) {
+        int[][] carteExtraite = {cartes[indice]};
+        int[][] cartesRestantes = new int[cartes.length - 1][2];
+        int index = 0;
+
+        for (int i = 0; i < cartes.length; i++) {
+            if (i != indice) {
+                cartesRestantes[index] = cartes[i];
+                index++;
+            }
+        }
+
+        return new Object[]{carteExtraite, cartesRestantes};
     }
+
+
+
+
 }
+
+
+
--- src/main/java/org/blackJack/Main.java
+++ src/main/java/org/blackJack/Main.java
@@ -3,14 +3,30 @@ package org.blackJack;
 import java.util.ArrayList;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to BlackJaack!!");
+        System.out.println("Welcome to Blackjack!!");
 
-        ArrayList<ArrayList<Integer>> allCartes = Carte.getALLCartes();
+        int[][] allCartes = Carte.getALLCartes();
 
-        for (ArrayList<Integer> card : allCartes) {
-            System.out.println("Card: " + card.get(0) + " of Form: " + card.get(1));
+        // Now you can do something with the allCartes array, like printing its contents
+        for (int i = 0; i < allCartes.length; i++) {
+            int value = allCartes[i][0];
+            int form = allCartes[i][1];
+            System.out.println("Card: " + value + " of Form: " + form);
         }
+
+        // Call the extraire_ieme_carte method
+        int indice = 2;
+        Object[] extractionResult = Carte.extraire_ieme_carte(allCartes, indice);
+        int[][] carteExtraite = (int[][]) extractionResult[0];
+        int[][] cartesRestantes = (int[][]) extractionResult[1];
+        System.out.println("Carte extraite: [" + carteExtraite[0][0] + " " + carteExtraite[0][1] + "]");
+        System.out.println("Cartes restantes: " + java.util.Arrays.deepToString(cartesRestantes));
+
+
     }
+
 }
 
