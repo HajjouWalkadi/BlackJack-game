bba8139e5ba6bd53576697e6f7ca8abd3572f3bf
hajjou walkadi <walkadihajjou153@gmail.com>
2023-10-05 14:10:37 +0100

    Shuffle the deck and implement player's turn logic

--- src/main/java/org/blackJack/Game.java
+++ src/main/java/org/blackJack/Game.java
@@ -8,31 +8,41 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         String playAgain;
 
+        do {
+            System.out.println("************************************************************************************************************************************************");
+            System.out.println("\n" +
+                    " __    __    ___  _        __   ___   ___ ___    ___      ______   ___       ____   _       ____    __  __  _   ____   ____    __  __  _ \n" +
+                    "|  |__|  |  /  _]| |      /  ] /   \\ |   |   |  /  _]    |      | /   \\     |    \\ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]\n" +
+                    "|  |  |  | /  [_ | |     /  / |     || _   _ | /  [_     |      ||     |    |  o  )| |    |  o  | /  / |  ' /  |__  ||  o  | /  / |  ' / \n" +
+                    "|  |  |  ||    _]| |___ /  /  |  O  ||  \\_/  ||    _]    |_|  |_||  O  |    |     || |___ |     |/  /  |    \\  __|  ||     |/  /  |    \\ \n" +
+                    "|  `  '  ||   [_ |     /   \\_ |     ||   |   ||   [_       |  |  |     |    |  O  ||     ||  _  /   \\_ |     \\/  |  ||  _  /   \\_ |     \\\n" +
+                    " \\      / |     ||     \\     ||     ||   |   ||     |      |  |  |     |    |     ||     ||  |  \\     ||  .  |\\  `  ||  |  \\     ||  .  |\n" +
+                    "  \\_/\\_/  |_____||_____|\\____| \\___/ |___|___||_____|      |__|   \\___/     |_____||_____||__|__|\\____||__|\\_| \\____||__|__|\\____||__|\\_|\n" +
+                    "                                                                                                                                         \n");
+            System.out.println("************************************************************************************************************************************************");
 
-        System.out.println("************************************************************************************************************************************************");
-        System.out.println("\n" +
-                " __    __    ___  _        __   ___   ___ ___    ___      ______   ___       ____   _       ____    __  __  _   ____   ____    __  __  _ \n" +
-                "|  |__|  |  /  _]| |      /  ] /   \\ |   |   |  /  _]    |      | /   \\     |    \\ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]\n" +
-                "|  |  |  | /  [_ | |     /  / |     || _   _ | /  [_     |      ||     |    |  o  )| |    |  o  | /  / |  ' /  |__  ||  o  | /  / |  ' / \n" +
-                "|  |  |  ||    _]| |___ /  /  |  O  ||  \\_/  ||    _]    |_|  |_||  O  |    |     || |___ |     |/  /  |    \\  __|  ||     |/  /  |    \\ \n" +
-                "|  `  '  ||   [_ |     /   \\_ |     ||   |   ||   [_       |  |  |     |    |  O  ||     ||  _  /   \\_ |     \\/  |  ||  _  /   \\_ |     \\\n" +
-                " \\      / |     ||     \\     ||     ||   |   ||     |      |  |  |     |    |     ||     ||  |  \\     ||  .  |\\  `  ||  |  \\     ||  .  |\n" +
-                "  \\_/\\_/  |_____||_____|\\____| \\___/ |___|___||_____|      |__|   \\___/     |_____||_____||__|__|\\____||__|\\_| \\____||__|__|\\____||__|\\_|\n" +
-                "                                                                                                                                         \n");
-        System.out.println("************************************************************************************************************************************************");
 
+            int[][] deck = Carte.getALLCartes();
+            int[][] playerHand = new int[0][2];
+            int[][] dealerHand = new int[0][2];
+            int playerScore = 0;
+            int dealerScore = 0;
+            boolean gameOver = false;
+            boolean playerTurn = true;
 
-        int[][] deck = Carte.getALLCartes();
-        int[][] playerHand = new int[0][2];
-        int[][] dealerHand = new int[0][2];
-        int playerScore = 0;
-        int dealerScore = 0;
-        boolean gameOver = false;
-        boolean playerTurn = true;
+            // Shuffle the deck
+            deck = Carte.melanger_jeu_cartes(deck);
 
-        // Shuffle the deck
-        deck = Carte.melanger_jeu_cartes(deck);
+            while (!gameOver) {
+                if (playerTurn) {
+                    System.out.println("\nPlayer's turn:");
+                    System.out.println("Your hand: ");
+                    playerHand = Carte.defausser_cartes(playerHand, playerHand);
+                    playerHand = Carte.defausser_cartes(playerHand, Carte.piocher_n_cartes(deck, 2)[0]);
+                    playerScore = calculateHandValue(playerHand);
+                    printHand(playerHand, playerScore);
 
+        }
     }
 }
 
